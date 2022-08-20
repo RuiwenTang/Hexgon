27508a5098cce8aa69435637e1360eb910b28f3b
RuiwenTang <tangruiwen1989@gmail.com>
2022-08-20 18:14:27 +0800

    Empty imgui render loop

--- SandBox/hello_imgui.cc
+++ SandBox/hello_imgui.cc
@@ -21,6 +21,8 @@
  *   SOFTWARE.
  */
 
+#include <imgui.h>
+
 #include <Hexgon/Hexgon.hpp>
 
 using namespace hexgon;
@@ -31,15 +33,79 @@ class ImguiLayer : public Layer {
   ~ImguiLayer() override = default;
 
  protected:
-  void OnAttach() override { HEX_INFO("ImguiLayer OnAttach"); }
+  void OnAttach() override {
+    HEX_INFO("ImguiLayer OnAttach");
+    InitImGui();
+  }
 
-  void OnUpdate(float tm) override {}
+  void OnUpdate(float tm) override {
+    ImGuiIO& io = ImGui::GetIO();
 
-  void OnDetach() override { HEX_INFO("ImguiLayer OnDetach"); }
+    io.DisplaySize.x = GetApplication()->GetWindow()->GetWidth();
+    io.DisplaySize.y = GetApplication()->GetWindow()->GetHeight();
+
+    ImGui::NewFrame();
+    DrawImGui();
+    ImGui::Render();
+    FlushImGui();
+  }
+
+  void OnDetach() override {
+    HEX_INFO("ImguiLayer OnDetach");
+    ImGui::DestroyContext();
+
+    m_font_texture.reset();
+  }
 
   void OnEvent(const Event* event) override {}
 
  private:
+  void InitImGui() {
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO();
+    // Fonts
+    // io.Fonts->AddFontFromFileTTF(RESOURCES_DIR "/Fonts/DroidSans.ttf", 16.f);
+
+    uint8_t* pixels;
+    int32_t width, height;
+    io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);
+
+    m_font_texture = GetGraphicsContext()->CreateTexture(
+        gpu::PixelFormat::R8G8B8A8UNormInt, gpu::TextureUsage::ShaderRead, gpu::TextureType::kImageTexture);
+  
+    m_font_texture->Resize(width, height);
+
+    m_font_texture->UploadData(0, 0, width, height, pixels);
+
+    io.Fonts->SetTexID(reinterpret_cast<ImTextureID>(m_font_texture.get()));
+  }
+
+  void DrawImGui() {
+    ImGui::Begin("Hello, World!");
+
+    bool checked = false;
+    ImGui::Checkbox("Check box 1", &checked);
+
+    ImGui::End();
+  }
+
+  void FlushImGui() {
+    ImDrawData* draw_data = ImGui::GetDrawData();
+
+    if (draw_data->TotalVtxCount > 0) {
+      size_t vertex_size = draw_data->TotalVtxCount * sizeof(ImDrawVert);
+      size_t index_size = draw_data->TotalIdxCount * sizeof(ImDrawIdx);
+
+      HEX_INFO("ImGui draw with vertex_count: {} and index_count: {} ", draw_data->TotalVtxCount,
+               draw_data->TotalIdxCount);
+      ;
+    }
+  }
+
+ private:
+  std::unique_ptr<gpu::Texture> m_font_texture;
 };
 
 int main(int argc, const char** argv) {
